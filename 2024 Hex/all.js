f69888631743f0e53cd597446e7465b104d1210e
ChenKuanHua <33916763+ChenKuanHua@users.noreply.github.com>
2024-05-22 21:18:25 +0800

    Add files via upload

---
+++ 2024 Hex/all.js
@@ -0,0 +1,7 @@
+$(".hamberger,.close").click(function() {
+    $(".phone-menu").toggleClass("show");
+    $(".blur-background").toggleClass("show");
+    $(".hamberger").toggleClass("disapear");
+    $(".close").toggleClass("disapear");
+
+})
